807480175bd2f27dad4a3d8832a7e7642050ac80
J ジ <89742343+Kuefo@users.noreply.github.com>
2024-11-29 23:20:10 -0800

    Add files via upload

---
+++ latest source/README.md
@@ -0,0 +1,12 @@
+# BO2 T6ZM Killcams (+ Mod Menu)
+my zombie killcam mod that is a work-in-progress and open sourced. this may work off plutonium but this was only supported for plutonium in the first place.
+
+## download
+you can download the latest version here:</br>
+<https://github.com/mjkzy/t6-zm-killcams/releases/tag/latest>
+
+## credits
+cmtfrosty - menu base  
+cabcon - bits of code here and there but not major  
+birchy - killcam help & killcam overlay  
+chase - github actions & auto compile
---
+++ og source/tools/compile.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal enableDelayedExpansion
+setlocal enableExtensions
+
+:: clear out any gsc, zip, and tmp/ files from previous runs
+del *.gsc /s /f /q > nul 2>&1
+del *.zip /s /f /q > nul 2>&1
+rd /s /q tmp > nul 2>&1
+
+mkdir tmp
+
+:: put main gscs into one file
+set "crt_dir=%~dp0"
+for %%I in ("%crt_dir%\..") do set "root=%%~fI"
+set "SEARCHPATH=%root%\src\gsc"
+
+for /F %%x in ('dir /B/D "%SEARCHPATH%\*.gsc"') do (
+  type %SEARCHPATH%\%%x >> %cd%\tmp\main.gsc
+)
+
+set FileName=%1
+
+:: copy all folders from .\src\gsc\ into %cd%\temp\
+for /F %%i in ('dir /a:d /b "%SEARCHPATH%\*"') do (
+  robocopy %SEARCHPATH%\%%i %cd%\tmp\%%i /s /e > nul
+)
+
+:: compile all files from temp
+gsc-tool.exe comp t6 %cd%\tmp\
+
+echo - compiled gsc
+
+:: type setup.txt into tmp
+echo %cd%\setup.txt
+type %cd%\setup.txt >> %cd%\tmp\setup.txt
+
+:: ZIP all the gsc together
+start /wait 7za a %FileName% .\tmp\* > nul
+echo - zipped gsc
